69770b9319a019686f3c953543a27a3ec2b56530
urosh <uros.damnjanovic@gmail.com>
2014-10-03 11:18:16 +0300

    Completing  event handlers refactoring

--- script/app.js
+++ script/app.js
@@ -2,96 +2,74 @@ angular.module('myApp', []);
 
 angular.module('myApp')
 	.directive('myAnnotation', ['$document', function($document){
-		var imgElement = angular.element('<img class="img-mask" src="img/brusli.jpeg">');
+		
+		var imgElement = angular.element('<img ng-mousedown="mouseDownHandler($event, \'img\')" class="img-mask" src="img/brusli.jpeg">');
+		
 		var spanElements = [];
 
-		var span_tl = angular.element("<span class='handler top-left'></span>");
-		var span_tr = angular.element("<span class='handler top-right'></span>");
-		var span_bl = angular.element("<span class='handler bottom-left'></span>");
-		var span_br = angular.element("<span class='handler bottom-right'></span>");
+		var span_tl = angular.element("<span ng-mousedown='mouseDownHandler($event, \"tl\")' class='handler top-left'></span>");
+		var span_tr = angular.element("<span ng-mousedown='mouseDownHandler($event, \"tr\")' class='handler top-right'></span>");
+		var span_bl = angular.element("<span ng-mousedown='mouseDownHandler($event, \"bl\")' class='handler bottom-left'></span>");
+		var span_br = angular.element("<span ng-mousedown='mouseDownHandler($event, \"br\")' class='handler bottom-right'></span>");
 		
-		var maskElement = angular.element('<div class="mask-div" ng-click="maskSelected($event)"></div>');
+		var maskElement = angular.element('<div class="mask-div"></div>');
 		
+		
+
 		var link = function(scope, elm){
-			var startX, startY, initialMouseX, initialMouseY, w, h, initialW, initialH, dx, dy, top, left;	
+			var startX, startY, initialMouseX, initialMouseY, 
+				w, h, initialW, initialH, dx, 
+				dy, top, left, source;	
+			
 			initialW = 180;
 			initialH = 110;	
 			
-			function mouseDownHandler($event, source){
+			scope.mouseDownHandler = function($event, _source_){
 				startX = maskElement.prop('offsetLeft');
 				startY = maskElement.prop('offsetTop');
 				
 				initialMouseX = $event.clientX;
         initialMouseY = $event.clientY;
 
-        if(source!='img'){
-        	$document.unbind('mousemove', mousemove);
-        	$document.unbind('mouseup', mouseup);
-        }
+        source = _source_;
+
+        $document.bind('mousemove', mousemove);
+        $document.bind('mouseup', mouseup);
 
 			}
 
+			function mousemove($event) {
+      	switch (source){
+      		case 'img':
+      			coef = [1, 1, 0, 0];
+      			break;
+      		case 'tl':
+      			coef = [1, 1, -1, -1];
+      			break;
+      		case 'tr':
+      			coef = [0, 1, 1, -1];
+      			break;
+      		case 'bl': 
+      			coef = [1, 0, -1, 1];
+      			break;
+      		case 'br':
+      			coef = [0, 0, 1, 1];
+      			break;
 
-			imgElement.bind('mousedown', function($event) {
-       	
-       	mouseDownHandler($event, 'img');
-			
-				$document.bind('mousemove', mousemove);
-        $document.bind('mouseup', mouseup);
+      	}
       
-        return false;
-      });
-
-
-			
-			
-			span_tl.bind('mousedown', function($event){
-				
-        mouseDownHandler($event, 'tl');
-				
-        $document.bind('mousemove',mousemove_tl);
-        $document.bind('mouseup', mouseup_tl);
-       
-			});
-
-			span_tr.bind('mousedown', function($event){
-				mouseDownHandler($event, 'tr');
-				
-        $document.bind('mousemove', mousemove_tr);
-        $document.bind('mouseup', mouseup_tr);
-       
-			});
-
-			span_bl.bind('mousedown', function($event){
-				
-				mouseDownHandler($event, 'bl');
-				
-        $document.bind('mousemove', mousemove_bl);
-        $document.bind('mouseup', mouseup_bl);
-       
-			});
-
-			span_br.bind('mousedown', function($event){
-				
-				mouseDownHandler($event, 'br');
-	
-        $document.bind('mousemove', mousemove_br);
-        $document.bind('mouseup', mouseup_br);
-       
-			});
-
-
-
-			function mouseMoveHandler($event, coef){
-				dx = $event.clientX - initialMouseX;
+      	dx = $event.clientX - initialMouseX;
         dy = $event.clientY - initialMouseY;
         
         left = startX + coef[0] * dx;
         top = startY + coef[1] * dy;
-       
+       	if( left < 0 ) { left = 0; }
+       	if( top < 0 )	{ top = 0; }
         w = initialW + coef[2] * dx;
         h = initialH + coef[3] * dy;
 
+        if ( (left + w) > 400 ) { w = 400 - left;}
+        
         maskElement.css({
           top:  top + 'px',
           left: left + 'px',
@@ -107,94 +85,20 @@ angular.module('myApp')
         	
         });
 
-			}
 
-
-
-			function mousemove_tl($event){
-				mouseMoveHandler($event, [1, 1, -1, -1]);
-				return false;
-			};
-
-			function mousemove_tr($event){
-        mouseMoveHandler($event, [0, 1, 1, -1]);
-        return false;
-     	};
-
-
-
-
-			function mousemove_bl($event){
-        mouseMoveHandler($event, [1, 0, -1, 1]);
-        return false;
-     	};
-
-    	function mousemove_br($event){
-				mouseMoveHandler($event, [0, 0, 1, 1]);
-				return false;
-      };
-
-    	function mousemove($event) {
-      	mouseMoveHandler($event, [1, 1, 0, 0]);
         return false;
       };
-
-
-
-
-
-			
-     
-
-
-
-
-
-
-      
-      function setSize(){
-      	initialW = w;
-      	initialH = h;
-      	
-      }
-      
-      function mouseup_tl($event){
-      	setSize();
-
-      	$document.unbind('mousemove', mousemove_tl);
-        $document.unbind('mouseup', mouseup_tl);
-      }
-
-      function mouseup_tr($event){
-      	setSize();
-      	
-      	$document.unbind('mousemove', mousemove_tr);
-        $document.unbind('mouseup', mouseup_tr);
-      }
-
-
-      function mouseup_bl($event){
-      	setSize();
-      	
-      	$document.unbind('mousemove', mousemove_bl);
-        $document.unbind('mouseup', mouseup_bl);
-      }
-
-      function mouseup_br($event){
-      	setSize();
-      	
-      	$document.unbind('mousemove', mousemove_br);
-        $document.unbind('mouseup', mouseup_br);
-      }
-
-
-
-      
 
       function mouseup($event){
+      	initialW = w;
+      	initialH = h;
+
       	$document.unbind('mousemove', mousemove);
         $document.unbind('mouseup', mouseup);
       };
+
+
+
 
 
 		};
@@ -205,10 +109,7 @@ angular.module('myApp')
 			template: '<div class="annotation-container"><img class="img-original" src="img/brusli.jpeg"></div>',
 			controller: function($scope){
 				
-				$scope.maskSelected = function($event){
-				
-				};
-
+			
 			},
 			compile: function(tElem){
 				maskElement.append(span_tl);
